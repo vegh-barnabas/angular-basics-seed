0b75068f0cbd6f02ae1b82a55d08dfba74a634c9
Barnabas Vegh <Barnabas.Vegh@quest.com>
2025-09-26 19:06:36 +0200

    create donut-single component, use instead of donut-list in app component

---
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'donut-form',
+  template: ` <form class="donut-form" #form="ngForm"></form> `,
+  styles: [],
+})
+export class DonutFormComponent {}
---
+++ src/app/admin/contactiners/donut-single/donut-single.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'donut-single',
+  template: `<donut-form></donut-form>`,
+  styles: [],
+})
+export class DonutSingleComponent {}

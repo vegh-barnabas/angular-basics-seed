e254cf12b357a71b0a6152487642d3ee016e9025
Barnabas Vegh <Barnabas.Vegh@quest.com>
2025-09-26 19:07:33 +0200

    fix: put formsModule in correct module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 // containers
 import { DonutListComponent } from './containers/donut-list/donut-list.component';
@@ -16,7 +17,7 @@ import { DonutFormComponent } from './components/donut-form/donut-form.component
     DonutCardComponent,
     DonutFormComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [DonutSingleComponent],
 })
 export class AdminModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, AdminModule],
+  imports: [BrowserModule, AdminModule],
   bootstrap: [AppComponent],
   exports: [],
 })

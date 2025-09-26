e1fba9c76ca154d6c30aa224876b8c8ff35f2def
Barnabas Vegh <Barnabas.Vegh@quest.com>
2025-09-26 20:07:51 +0200

    add form to donut form component, add some styling

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -2,7 +2,94 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'donut-form',
-  template: ` <form class="donut-form" #form="ngForm"></form> `,
-  styles: [],
+  template: `
+    <form class="donut-form" #form="ngForm">
+      <label>
+        <span>Name</span>
+        <input type="text" class="input" name="Name" required ngModel />
+      </label>
+      <label>
+        <span>Icon</span>
+        <select name="icon" class="input input--select" required ngModel>
+          <option *ngFor="let icon of icons" [ngValue]="icon">
+            {{ icon }}
+          </option>
+        </select>
+      </label>
+      <label>
+        <span>Price</span>
+        <input type="number" class="input" name="price" required ngModel />
+      </label>
+      <div class="donut-form-radios">
+        <p class="donut-form-radios-label">Promo:</p>
+        <label>
+          <input
+            type="radio"
+            name="promo"
+            required
+            [value]="undefined"
+            ngModel
+          />
+          <span>None</span>
+        </label>
+        <label>
+          <input type="radio" name="promo" required value="new" ngModel />
+          <span>New</span>
+        </label>
+        <label>
+          <input type="radio" name="promo" required value="limited" ngModel />
+          <span>Limited</span>
+        </label>
+      </div>
+
+      <label>
+        <span>Description</span>
+        <textarea
+          name="description"
+          class="input input--textarea"
+          required
+          ngModel
+        ></textarea>
+      </label>
+
+      {{ form.value | json }}
+    </form>
+  `,
+  styles: [
+    `
+      .donut-form {
+        &-radios {
+          display: flex;
+          align-content: center;
+
+          &-label {
+            margin-right: 10px;
+          }
+
+          label {
+            display: flex;
+            align-items: center;
+
+            span {
+              color: #444;
+              margin-bottom: 0;
+            }
+          }
+        }
+      }
+    `,
+  ],
 })
-export class DonutFormComponent {}
+export class DonutFormComponent {
+  icons: string[] = [
+    'caramel-swirl',
+    'glazed-fudge',
+    'just-chocolate',
+    'sour-supreme',
+    'strawberry-glaze',
+    'vanilla-sundae',
+    'zesty-lemon',
+  ];
+
+  constructor() {}
+}

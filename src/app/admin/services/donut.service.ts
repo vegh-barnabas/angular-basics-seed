060c99cf43ad0f857fa439170576ec31a2663157
vegh-barnabas <62122932+vegh-barnabas@users.noreply.github.com>
2025-10-17 21:28:58 +0100

    use retry with delay

--- src/app/admin/services/donut.service.ts
+++ src/app/admin/services/donut.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { tap, of, map, catchError, throwError, retry } from 'rxjs';
+import { tap, of, map, catchError, throwError, retry, timer } from 'rxjs';
 
 import { Donut } from '../models/donut.model';
 
@@ -60,7 +60,7 @@ export class DonutService {
           return item;
         });
       }),
-      retry(2),
+      retry({ count: 2, delay: () => timer(1000) }),
       catchError((error) => this.handleError(error))
     );
   }
